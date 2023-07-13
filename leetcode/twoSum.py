8752fd964507e14be4a548aa0e1d11e39746f343
sonumahajan <mahajan00sonu@gmail.com>
2023-07-13 21:11:33 +0530

    two sum problem

--- leetcode/twoSum.py
+++ leetcode/twoSum.py
@@ -1,17 +1,19 @@
-# class Solution:
-def twoSum(nums, target):
-    result = []
-    for i in range(len(nums)):
-        for j in range(i,len(nums)):
-            sum = nums[i]+nums[j]
-            if(sum == target):
-                if(i != j and i not in result and j not in result):
-                    result.append(i)
-                    result.append(j)
-    return result
+class Solution:
+    def twoSum(self, nums, target):
+        result = []
+        for i in range(len(nums)):
+            for j in range(i,len(nums)):
+                sum = nums[i]+nums[j]
+                if(sum == target):
+                    if(i != j and i not in result and j not in result):
+                        result.append(i)
+                        result.append(j)
+        return result
 
-# Solution s1 = object Solution()
-print(twoSum([2,7,11,15], 9))
-print(twoSum([3,2,4], 6))
-print(twoSum([3,3], 6))
+obj = Solution()
+# result = obj.twoSum([2,7,11,15], 9)
+# result = obj.twoSum([3,2,4], 6)
+result = obj.twoSum([3,3], 6)
+
+print(result)
 
